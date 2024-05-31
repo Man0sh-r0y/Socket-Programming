31fe3f2eeb0bcae7a10981196a3c95ad57e8ad47
Manash Roy <manashroy454@gmail.com>
2024-05-31 20:39:02 +0530

    updated the file

--- udpClient.c
+++ udpClient.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <stdio.h>          // Standard I/O library for functions like printf and perror
+#include <stdlib.h>         // Standard library for functions like exit and atoi
+#include <string.h>         // String handling library for functions like memset, bzero, and strcpy
+#include <sys/socket.h>     // Library for socket functions
+#include <sys/types.h>      // Library for data types used in socket programming
+#include <netinet/in.h>     // Library for Internet address family
+#include <arpa/inet.h>      // Library for functions like inet_addr and htons
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) // Main function with command line arguments
 {
-
+    // Check if the correct number of arguments is provided
     if (argc != 2)
     {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(0);
+        printf("Usage: %s <port>\n", argv[0]); // Print usage message
+        exit(0); // Exit the program if the number of arguments is incorrect
     }
 
-    char *ip = "127.0.0.1";
-    int port = atoi(argv[1]);
+    char *ip = "127.0.0.1"; // IP address to bind the server to (localhost)
+    int port = atoi(argv[1]); // Convert the port number from a string to an integer
 
-    int sockfd;
-    struct sockaddr_in addr;
-    char buffer[1024];
-    socklen_t addr_size;
+    int sockfd; // Socket file descriptor
+    struct sockaddr_in addr; // Structure to hold the address information
+    char buffer[1024]; // Buffer to hold data
+    socklen_t addr_size; // Variable to hold the size of the address
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(ip);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // Create a UDP socket
+    memset(&addr, '\0', sizeof(addr)); // Clear the address structure
+    addr.sin_family = AF_INET; 
+    addr.sin_port = htons(port); 
+    addr.sin_addr.s_addr = inet_addr(ip); 
 
-    bzero(buffer, 1024);
-    strcpy(buffer, "Hello, World!");
-    sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, sizeof(addr));
-    printf("[+]Data send: %s\n", buffer);
+    bzero(buffer, 1024); // Clear the buffer
+    strcpy(buffer, "Hello, World!"); // Copy the message into the buffer
+    sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, sizeof(addr)); // Send the message to the server
+    printf("[+]Data send: %s\n", buffer); // Print the sent message
 
-    bzero(buffer, 1024);
-    addr_size = sizeof(addr);
-    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, &addr_size);
-    printf("[+]Data recv: %s\n", buffer);
+    bzero(buffer, 1024); // Clear the buffer again
+    addr_size = sizeof(addr); // Set the size of the address structure
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, &addr_size); // Receive data from the server
+    printf("[+]Data recv: %s\n", buffer); // Print the received data
 
     return 0;
 }
--- udpServer.c
+++ udpServer.c
@@ -1,57 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <stdio.h>          // Standard I/O library for functions like printf and perror
+#include <stdlib.h>         // Standard library for functions like exit and atoi
+#include <string.h>         // String handling library for functions like memset and strcpy
+#include <sys/socket.h>     // Library for socket functions
+#include <sys/types.h>      // Library for data types used in socket programming
+#include <netinet/in.h>     // Library for Internet address family
+#include <arpa/inet.h>      // Library for functions like inet_addr and htons
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) // Main function with command line arguments
 {
-
-    if (argc != 2)
+    if (argc != 2) // Check if the correct number of arguments is provided
     {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(0);
+        printf("Usage: %s <port>\n", argv[0]); // Print usage message
+        exit(0); // Exit the program if the number of arguments is incorrect
     }
 
-    char *ip = "127.0.0.1";
-    int port = atoi(argv[1]);
+    char *ip = "127.0.0.1"; // IP address to bind the server to (localhost)
+    int port = atoi(argv[1]); // Convert the port number from a string to an integer
 
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[1024];
-    socklen_t addr_size;
-    int n;
+    int sockfd; // Socket file descriptor
+    struct sockaddr_in server_addr, client_addr; // Structures to hold server and client addresses
+    char buffer[1024]; // Buffer to hold data
+    socklen_t addr_size; // Variable to hold the size of the address
+    int n; // Variable for return values
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0)
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // Create a UDP socket
+    if (sockfd < 0) // Check if the socket was created successfully
     {
-        perror("[-]socket error");
-        exit(1);
+        perror("[-]socket error"); // Print an error message if socket creation failed
+        exit(1); // Exit the program
     }
 
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    memset(&server_addr, '\0', sizeof(server_addr)); // Clear the server address structure
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_port = htons(port); 
+    server_addr.sin_addr.s_addr = inet_addr(ip); 
 
-    n = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (n < 0)
+    n = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)); // Bind the socket to the IP address and port
+    if (n < 0) // Check if binding was successful
     {
-        perror("[-]bind error");
-        exit(1);
+        perror("[-]bind error"); // Print an error message if binding failed
+        exit(1); // Exit the program
     }
 
-    bzero(buffer, 1024);
-    addr_size = sizeof(client_addr);
-    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_size);
-    printf("[+]Data recv: %s\n", buffer);
+    bzero(buffer, 1024); // Clear the buffer
+    addr_size = sizeof(client_addr); // Set the size of the client address structure
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_size); // Receive data from a client
+    printf("[+]Data recv: %s\n", buffer); // Print the received data
 
-    bzero(buffer, 1024);
-    strcpy(buffer, "Welcome to the UDP Server.");
-    sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
-    printf("[+]Data send: %s\n", buffer);
+    bzero(buffer, 1024); // Clear the buffer again
+    strcpy(buffer, "Welcome to the UDP Server."); // Copy a welcome message into the buffer
+    sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, sizeof(client_addr)); // Send the welcome message to the client
+    printf("[+]Data send: %s\n", buffer); // Print the sent message
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
